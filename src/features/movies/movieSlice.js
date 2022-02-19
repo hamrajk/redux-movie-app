160fb955bc333cfbb145ad7e1afe2fb17b5d2bbe
hamrajk <hamraj_khangura@hotmail.com>
2022-02-18 21:42:56 -0500

    Added detailed info section for each movie, with state managed through redux using both synchronous and asynchronous actions

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -42,8 +42,8 @@ const movieSlice = createSlice({
   name: "movies",
   initialState,
   reducers: {
-    addMovies: (state, { payload }) => {
-      state.movies = payload;
+    removeSelectedMovieOrShow: (state) => {
+      state.selectedMovieOrShow = {};
     },
   },
   extraReducers: {
@@ -68,7 +68,7 @@ const movieSlice = createSlice({
   },
 });
 
-export const { addMovies } = movieSlice.actions;
+export const { removeSelectedMovieOrShow } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
 export const getSelectedMovieOrShow = (state) =>
